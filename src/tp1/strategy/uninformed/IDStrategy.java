565e6c073c81535f108cdf6fd5e2c9ea7d1368a8
Dario Susnisky <dario15th@gmail.com>
2013-03-23 17:40:12 -0300

    ID :D :D :D :D: D: :D :D :D
    You can play to find the weird smile

--- src/tp1/strategy/uninformed/IDStrategy.java
+++ src/tp1/strategy/uninformed/IDStrategy.java
@@ -1,18 +1,26 @@
 package tp1.strategy.uninformed;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
 import java.util.Stack;
 
 import tp1.engine.Node;
 import tp1.problem.Game;
+import tp1.problem.State;
 
 public class IDStrategy extends UninformedStrategy {
 
 	int step = 1;
 	Stack<Node> openedDFS = new Stack<Node>();
+	private Set<State> openedHash = new HashSet<State>();
+	private Queue<Node> openedBFS = new LinkedList<Node>();
 	
 	public IDStrategy(Game game) {
 		
 		super(game);
+		openedBFS.add(null);
 	}
 
 	@Override
@@ -23,30 +31,48 @@ public class IDStrategy extends UninformedStrategy {
 
 	@Override
 	public void addOpenedNode(Node node) {
-		// TODO Auto-generated method stub
-
+		
+		if ((node.getLevel() == 0) || (node.getLevel() % this.step == 0)) {
+			
+			openedDFS.push(node);
+		} else {
+			
+			State state = node.getState();
+			
+			if (!openedHash.contains(state)) {
+				
+				openedBFS.add(node);
+				openedHash.add(state);
+			}
+		}
 	}
 
 	@Override
 	public Node popOpenedNode() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		if (!openedDFS.isEmpty()) {
+			
+			return openedDFS.pop();
+		}
+		
+		if (openedBFS.isEmpty()) {
+			return null;
+		}
+		
+		Node node = openedBFS.poll();
+		
+		if (node != null) {
+			return node;
+		}
+		
+		openedHash.clear();
+		openedBFS.add(null);
+		
+		return openedBFS.poll();
 	}
 
 	public void setStep(int step) {
 		
 		this.step = step;
-	}
-
-	class IDNode {
-		
-		public Node node;
-		public int level;
-		
-		public IDNode(Node node, int level) {
-			
-			this.node = node;
-			this.level = level;
-		}
 	}
 }
