2acc58b618172b7e5888dbf90df0f10e2e48f367
esneider <dariosn@gmail.com>
2013-03-25 13:25:17 -0300

    tildes informe

--- doc/make.sh
+++ doc/make.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-gcc -I. -E -x c -o informe_completo.html.bak informe.html
+gcc -I. -w -E -x c -o informe_completo.html.bak informe.html
 cat informe_completo.html.bak | egrep -v "^#.*$" | sed "s/@/#/g" > informe_completo.html
 rm informe_completo.html.bak
